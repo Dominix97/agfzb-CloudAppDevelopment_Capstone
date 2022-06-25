802bd46c0ff1f98d16cac1c6cbacc108c6a2e1e3
Dominix97 <dominic.michael97@gmail.com>
2022-06-25 06:26:56 +0000

    Models Changes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,24 +4,37 @@ from django.utils.timezone import now
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+# <HINT> Create a Car Make model `
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    name2 = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "Name: " + self.name
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'SEDAN'
+    SUV = 'SUV'
+    WAGON = 'WAGON'
+
+    CAR_TYPE_CHOICES = (
+        (SEDAN, 'SEDAN'),
+        (SUV, 'SUV'),
+        (WAGON, 'WAGON')
+    )
+
+    carmakes = models.ManyToManyField(CarMake)
+    name = models.CharField(max_length=100)
+    dealer_id = models.IntegerField()
+    cartype = models.CharField(max_length=20,choices=CAR_TYPE_CHOICES)
+    year = models.DateField()
+
+    def __str__(self):
+        return "Name: " + self.name
 
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+    # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
-
-# <HINT> Create a plain Python class `DealerReview` to hold review data
+    # <HINT> Create a plain Python class `DealerReview` to hold review data
